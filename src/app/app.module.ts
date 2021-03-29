b738f01ed80eaf48f9e5af729c876012a467d316
[arbaz.sohail@outlook.com] <63145796+arbaz-maker@users.noreply.github.com>
2021-03-29 23:04:31 +0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,37 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { CourselistComponent } from './courselist/courselist.component';
+import { StudentsComponent } from './students/students.component';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginpageComponent } from './loginpage/loginpage.component';
+
+
+const routes: Routes = [
+  { path: 'first-component', component: HomepageComponent },
+  { path: 'second-component', component: CourselistComponent },
+  { path: 'third-component', component: StudentsComponent },
+  { path: 'login-component', component: LoginpageComponent },
+];
+
+
 
 @NgModule({
+  
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    CourselistComponent,
+    StudentsComponent,
+    LoginpageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
